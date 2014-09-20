1dd743f0efedbbb8cbeebe9f5e39ef62ac4db1f9
Ivan Akulov <gxoptg@gmail.com>
2014-09-21 01:55:21 +0400

    Alias from /blocks to /public/blocks Nice error handler

--- app.js
+++ app.js
@@ -1,58 +1,51 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var lessMiddleware = require('less-middleware');
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var lessMiddleware = require("less-middleware");
 
 var userSideRouter = require("./routes/userside");
 var apiPostsRouter = require("./routes/api_posts");
 
+var AppError = require("./core/apperror");
+
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.locals.basedir = __dirname;
 
-app.use(logger('dev'));
-app.use(favicon());
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-//app.use(cookieParser());
-app.use(lessMiddleware(path.join(__dirname, 'public'), { force: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+// Prohibit access to .jade files
+app.use("*.jade", function(req, res, next) {
+    next(AppError.pageNotFound);
+});
+app.use("/blocks", lessMiddleware(path.join(__dirname, "blocks")));
+app.use(lessMiddleware(path.join(__dirname, "public")));
+// Alias /blocks to /public/blocks
+app.use("/blocks", express.static(path.join(__dirname, "blocks")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", userSideRouter);
 app.use("/api/posts", apiPostsRouter);
 
 /// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    next(AppError.pageNotFound);
 });
 
-/// error handlers
+// Error handler
+app.use(function (error, req, res, next) {
+    if (error.stack) {
+        console.error(error.stack);
+    }
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+    res.status(error.httpStatus);
+    res.render("error", {
+        error: error
     });
 });
 
